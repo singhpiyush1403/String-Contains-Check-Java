ea992f2bc2c4a8f3227c16479087619d1aa7d62c
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-04-18 22:15:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String args[]){
+        
+        String data1 = "Piyush Selenium";
+        String data = "Piyush";
+        if(data.contains(data1)){
+            System.out.println("Exit");
+        }
+        else{
+            System.out.println("Not Exit");
+        }
+    }
+}
